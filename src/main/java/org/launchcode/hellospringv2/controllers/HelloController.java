a73f4cacf91b2bb5ccc2cc9247a3c444a4031c1d
Your Name <you@example.com>
2020-03-24 18:35:50 -0500

    Finished intro to Controllers & Methods using Spring

--- src/main/java/org/launchcode/hellospringv2/controllers/HelloController.java
+++ src/main/java/org/launchcode/hellospringv2/controllers/HelloController.java
@@ -1,41 +1,42 @@
 package org.launchcode.hellospringv2.controllers;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 //there are methods in this class that will handle HTTP requests
 @Controller
+@ResponseBody
+@RequestMapping("hello")
 public class HelloController {
 
-        //handles requests at path /hello
-//    @GetMapping("hello")
-//    @ResponseBody
-//    public String hello(){
-//        return "Hello, Spring";
-//
-//    }
-
+    //lives at /hello/goodbye
     @GetMapping("goodbye")
-    @ResponseBody
     public String goodbye(){
         return "Goodbye, Spring!";
     }
 
     //Handles request of the form /hello?name=launchcode
-    @GetMapping("hello")
-    @ResponseBody
+    @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST}, value = "hello")
     public String helloWithQueryParam(@RequestParam String name){
         return "Hello, " + name + "!";
     }
 
     //Handles requests of the form /hello/Launchcode
-    @GetMapping("hello/{name}")
-    @ResponseBody
+    @GetMapping("{name}")
     public String helloWithPathParam(@PathVariable String name){
         return "Hello, " + name + "!";
+    }
+    //lives at /hello/form
+    @GetMapping("form")
+    public String helloForm(){
+        return "<html>" +
+                "<body>" +
+                "<form action='hello' method='post'>" +
+                "<input type='text' name = 'name'>" +
+                "<input type ='submit' value = 'Greet me!'>" +
+                "</form>" +
+                "</body>" +
+                "</html>";
     }
 }
 
